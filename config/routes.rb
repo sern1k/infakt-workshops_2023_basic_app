17547877ff33a75db810e5074dc2e35894051f62
Pola Madej <Pola.Madej@motorolasolutions.com>
2023-11-03 15:20:44 +0100

    dodanie oauth i wydarzenia w kalendarzu

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
     member do
       post 'cancel'
     end
-  end 
-  devise_for :users
+  end
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
