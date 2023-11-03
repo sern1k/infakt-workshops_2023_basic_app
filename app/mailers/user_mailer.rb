d229acd2ba78fab32a39f6402790e40b8b7a1a77
Pola Madej <Pola.Madej@motorolasolutions.com>
2023-11-03 22:17:01 +0100

    part2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,10 @@ class UserMailer < ApplicationMailer
     @date = book_loan.due_date
     mail(to: book_loan.user.email, subject: 'sub')
   end
+
+  def due_date_notification_email(book_loan)
+    @title = book_loan.book.title
+    @date = book_loan.due_date
+    mail(to: book_loan.user.email, subject: 'sub2')
+  end
 end
